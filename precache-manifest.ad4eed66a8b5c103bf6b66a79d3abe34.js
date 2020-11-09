9dba1d1fa0704582b88d4f0a9a9b1cdd34c70e23
Pedro Batuza <abraham_desmadroso@hotmail.com>
2020-11-09 09:23:22 -0600

    Updates

---
+++ precache-manifest.ad4eed66a8b5c103bf6b66a79d3abe34.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a19a8972aa3368580d188ee8e58f409",
+    "url": "/portafolioV2/index.html"
+  },
+  {
+    "revision": "0337761a478fefc719e6",
+    "url": "/portafolioV2/static/css/2.b4ad8931.chunk.css"
+  },
+  {
+    "revision": "224f4f6b07c7fb7128de",
+    "url": "/portafolioV2/static/css/main.48f46b1d.chunk.css"
+  },
+  {
+    "revision": "0337761a478fefc719e6",
+    "url": "/portafolioV2/static/js/2.95b2b943.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/portafolioV2/static/js/2.95b2b943.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "224f4f6b07c7fb7128de",
+    "url": "/portafolioV2/static/js/main.e6868304.chunk.js"
+  },
+  {
+    "revision": "6cb67be6cf4bce3bc563",
+    "url": "/portafolioV2/static/js/runtime-main.3bdef7e6.js"
+  },
+  {
+    "revision": "f8cd61b2d74d16ca94ac08ff9f5e514d",
+    "url": "/portafolioV2/static/media/dues.f8cd61b2.png"
+  },
+  {
+    "revision": "fc88fc1b716809f20b028b1937d92b94",
+    "url": "/portafolioV2/static/media/random.fc88fc1b.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portafolioV2/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portafolioV2/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portafolioV2/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portafolioV2/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "77641912d5a063747c956140453e3e1d",
+    "url": "/portafolioV2/static/media/índice.77641912.jpg"
+  }
+]);
